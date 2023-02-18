c8e8f507e8e33b702f68b1f4c88173d31931b203
DenisGraur <88393516+DenisGraur@users.noreply.github.com>
2023-02-18 13:15:21 +0000

    add an inner class ( non-static)

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import javax.print.event.PrintJobListener;
+
 
 public class Main
 {
@@ -8,8 +10,14 @@ public class Main
 		// create an object of the static nested class
        // using the name of the outer class:
        
-       Class1.InnerClass1 obj1 = new Class1.InnerClass1();
+       Class1.StaticClass1 obj1 = new Class1.StaticClass1();
 	   obj1.method1();
+
+       // create an object of the outer class:
+       Class1 obj2 = new Class1();
+        // create an object of the inner classs using the outer class:
+       Class1.InnerClass1 obj3 = obj2.new InnerClass1();
+       obj3.method2(); 
 	}
 }
 
@@ -19,11 +27,19 @@ public class Main
 class Class1 {
     
     // static class:
-    static class InnerClass1{
+    static class StaticClass1{
         int var1 = 10;
         int var2 = 20;
         public void  method1(){
             System.out.println(var1 + var2);
+            System.out.println("this is method1 from the static class");
+        }
+    }
+
+    // inner class:
+    class InnerClass1{
+        public void method2(){
+            System.out.println("This is method 2 from the non-static inner class");
         }
     }
 }
